42dde651df95b1867a18682e220f0831fec39bfd
Nirmal Kumar Gupta <nihalgupta.nirmal@gmail.com>
2025-06-19 20:51:50 +0530

    Introduced a new table view for meetings showing name, status, agent details, and readable duration. Added an empty state message for when no meetings are available. Enhanced data by including agent info and calculated meeting duration.

--- src/modules/agents/ui/components/columns.tsx
+++ src/modules/agents/ui/components/columns.tsx
@@ -1,12 +1,14 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
-import { GeneratedAvatar } from "@/components/generated-avatar"
-import { AgentGetOne } from "../../types"
 import { CornerDownRightIcon, VideoIcon } from "lucide-react"
-import { Badge } from "@/components/ui/badge"
 
-export const columns: ColumnDef<AgentGetOne>[] = [
+import { Badge } from "@/components/ui/badge"
+import { GeneratedAvatar } from "@/components/generated-avatar"
+
+import { AgentsGetMany } from "../../types"
+
+export const columns: ColumnDef<AgentsGetMany[number]>[] = [
   {
     accessorKey: "name",
     header: "Agent Name",
